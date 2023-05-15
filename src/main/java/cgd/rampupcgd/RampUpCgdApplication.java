081d2065a936b869564b00eb9d3b41337a7fc626
Luís Rodrigues <Luis.Rodrigues@criticalsoftware.com>
2023-05-15 23:41:02 +0100

    First commit

---
+++ src/main/java/cgd/rampupcgd/RampUpCgdApplication.java
@@ -0,0 +1,13 @@
+package cgd.rampupcgd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RampUpCgdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RampUpCgdApplication.class, args);
+	}
+
+}
